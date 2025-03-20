69976adbae040df4e875172f16467fdcabeb6200
Kashivi Goel <146112735+Kashivi@users.noreply.github.com>
2025-03-20 19:08:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from models.database import db
 
 def create_app():
-    app = Flask(__name__ , template_folder='templates/')
+    app = Flask(__name__ , template_folder='templates')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quiz_master.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.app_context().push()
